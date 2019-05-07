52e5f21d07f02068527ff5094823e931191a7105
nbgt82 <48896344+nbgt82@users.noreply.github.com>
2019-05-06 22:19:44 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,10 +1,10 @@
 <?php
- $destino="alexxytb19@gmail.com";
- $nombre=$_POST["name"];
- $correo=$_POST["email"];;
- $mensaje=$_POST["message"];
- $contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
- mail($destino,$Contacto, $contenido);
- header("Location: registro.html");
+ $destino= "alexxytb19@gmail.com";
+ $nombre = $_POST["nombre"];
+ $correo = $_POST["correo"];
+ $mensaje = $_POST["message"];
+ $contenido= "Nombre: " . $nombre . "\nCorreo : ". $correo . "\nMensaje : " .$mensaje;
+ mail($destino,"Contacto", $contenido);
+ header("Location:gracias.html");
 
 ?>
